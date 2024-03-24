0ddc78e3e6d4771f841c66263915d975c57fdc6d
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-24 16:04:31 -0700

    function to add Todos to existing project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import Todo from './modules/Todo.js';
+import Project from './modules/Project.js';
 import ProjectManager from './modules/ProjectManager.js';
 
 const _projectManager = new ProjectManager();
@@ -18,5 +19,10 @@ const testTodo = new Todo("testId", "testTitle", "testDescription", "testDueDate
 const testTodo2 = new Todo("testId2", "testTitle2", "testDescription2", "testDueDate2", "testPriority2", "testFinished2");
 const testTodoList = [testTodo, testTodo2];
 
-_projectManager.addProject("testProjectId", "testProjectName", testTodoList);
+_projectManager.addProject("testProjectName", "testProjectDescription", testTodoList);
+let testProjectId = _projectManager.projectList[0].id;
+console.log(testProjectId);
+
+_projectManager.addTodoToProject(testProjectId, "another todo name", "empty desc", "empty due date", "empty priority", true);
+
 console.log(_projectManager);
--- src/modules/ProjectManager.js
+++ src/modules/ProjectManager.js
@@ -1,12 +1,28 @@
 import Project from './Project.js';
+import Todo from './Todo.js';
 
 class ProjectManager {
     constructor() {
         this.projectList = [];
     }
 
-    addProject(id, name, todoList = []) {
-        this.projectList.push(new Project(id, name, todoList));
+    addProject(name, description, todoList = []) {
+        this.projectList.push(new Project(name, description, todoList));
+    }
+
+    addTodoToProject(projectId, title, description, dueDate, priority, isFinished = false) {
+        let projectIndex = -1;
+        for (let i = 0; i < this.projectList.length; i++) {
+            if (this.projectList[i].id === projectId) {
+                projectIndex = i;
+                break;
+            }
+        }
+        if (projectIndex === -1) {
+            console.log("O.o project not found wat");
+            return;
+        }
+        this.projectList[projectIndex].todoList.push(new Todo(title, description, dueDate, priority, isFinished));
     }
 }
 
