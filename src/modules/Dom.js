3d860097560da47b6f1d3a85ce94bb0562c0076e
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-03-28 16:30:41 -0700

    basic dialog form for adding new projects

--- src/modules/Dom.js
+++ src/modules/Dom.js
@@ -1,12 +1,38 @@
 import ProjectManager from "./ProjectManager.js";
 
+const page = document.getElementById("page-container");
+const addProjectDialog = document.getElementById("add-project-dialog");
+
+function createAddProjectDialog() {
+    addProjectDialog.innerHTML = `
+    <form id="add-project-form">
+        <div>
+            <p id="dialog-header">Create a new project</p>
+            <div>
+                <label for="input-project-name">Name:</label>
+                <input id="input-project-name" name="input-project-name" type="text" required>
+            </div>
+            <div>
+                <label for="input-project-description">Description:</label>
+                <input id="input-project-description" name="input-project-description" type="textarea">
+            </div>
+        </div>
+    <button id="submit-add-project" type="submit">Add project</button>
+    <button id="cancel-add-project" type="reset">Cancel</button>
+    </form>`;
+}
+
 function renderPage() {
+    createAddProjectDialog();
+    const closeAddProjectDialog = document.getElementById("cancel-add-project");
+    closeAddProjectDialog.addEventListener("click", () => addProjectDialog.close());
+    addProjectDialog.addEventListener("submit", addProject);
+
     const _projectManager = new ProjectManager();
-    const page = document.getElementById("page-container");
     
     const addProjectButton = document.createElement("button");
     addProjectButton.textContent = "Add new project";
-    addProjectButton.addEventListener("click", addProject);
+    addProjectButton.addEventListener("click", () => document.getElementById("add-project-dialog").showModal());
     page.appendChild(addProjectButton);
 
     const addTodoButton = document.createElement("button");
@@ -15,9 +41,12 @@ function renderPage() {
     page.appendChild(addTodoButton);
     
     function addProject() {
-        let name = prompt("Enter a project name:", "Project Name");
-        let description = prompt("Enter a project description", "Some description");
+        event.preventDefault();
+        addProjectDialog.close();
+        let name = document.getElementById("input-project-name").value;
+        let description = document.getElementById("input-project-description").value;
         _projectManager.addProject(name, description);
+        document.getElementById("add-project-form").reset();
     }
 
     function addTodoToProject() {
@@ -27,6 +56,7 @@ function renderPage() {
         let priority = prompt("Enter a priorit (1-5):", "3");
         _projectManager.addTodoToProject(projectID, description, dueDate, priority);
     }
+
 }
 
 export default renderPage;
